c808b0f05b162aefbc962f71dabc8c9af76be05e
Emily Blair <emilyblair96@gmail.com>
2021-02-21 12:59:42 -0600

    Add material ui data table

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import DataTable from './DataTable'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
